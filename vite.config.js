f77fd2b43a62b8fec9f1e1074dcc0d9a71c5ec51
Metaxona <110418886+Metaxona@users.noreply.github.com>
2023-08-03 12:26:26 +0000

    Move App To gh-pagess: (05d9267) Updated base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: 'https://metaxona.com/LearnWeb3_Whitelist_DApp/',
   plugins: [react()],
 })
